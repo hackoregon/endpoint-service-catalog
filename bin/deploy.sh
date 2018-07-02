ed533af92ef7716ebff9a2b8f63ab1d4b1dfcb2f
Mike Lonergan <mikethecanuck@gmail.com>
2018-07-01 23:22:32 -0700

    Updated to 2018 deploy.sh script used by all 2018 container repos

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,20 +1,37 @@
 #! /bin/bash
+
 # Tag, Push and Deploy only if it's not a pull request
-# Comment
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  # Push only if we're testing the master branch
-  if [ "$TRAVIS_BRANCH" == "master" ]; then
-    pip install --user awscli
-    export PATH=$PATH:$HOME/.local/bin
-    docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
-    docker push "$DOCKER_REPO"/"$DOCKER_IMAGE":latest
-    ./bin/ecs-deploy.sh  \
-     -n "$ECS_SERVICE_NAME" \
-     -c "$ECS_CLUSTER"   \
-     -i "$DOCKER_REPO"/"$DOCKER_IMAGE":latest
-   else
-     echo "Skipping deploy because branch is not master"
-  fi
+
+    # Push only if we're testing the master branch
+    if [ "$TRAVIS_BRANCH" == "master" ]; then
+
+        echo Getting the ECR login...
+        eval $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
+
+        REMOTE_DOCKER_PATH="$DOCKER_REPO"/"$DOCKER_REPO_NAMESPACE"/"$DOCKER_IMAGE"
+        
+        # tag with branch and travis build number then push
+        TAG=travis-buildnum-"$TRAVIS_BUILD_NUMBER"
+        echo Tagging with "$TAG"
+        docker tag "$DOCKER_IMAGE":latest "$REMOTE_DOCKER_PATH":"$TAG"    
+        docker push "$REMOTE_DOCKER_PATH":"$TAG"
+
+        # tag with "latest" then push
+        TAG=latest
+        echo Tagging with "$TAG"
+        docker tag "$DOCKER_IMAGE":latest "$REMOTE_DOCKER_PATH":"$TAG"
+        docker push "$REMOTE_DOCKER_PATH":"$TAG"
+        
+        #echo Running ecs-deploy.sh script...
+        bin/ecs-deploy.sh  \
+           --service-name "$ECS_SERVICE_NAME" \
+           --cluster "$ECS_CLUSTER"   \
+           --image "$REMOTE_DOCKER_PATH":latest \
+           --timeout 300
+    else
+        echo "Skipping deploy because branch is not master"
+    fi
 else
-  echo "Skipping deploy because it's a pull request"
+    echo "Skipping deploy because it's a pull request"
 fi
